5d202ffd84f33e875fe338700edce4a60f37c443
XYC710 <3246771261@qq.com>
2024-09-01 09:43:37 +0800

    Add files via upload

---
+++ import math;.py
@@ -0,0 +1,26 @@
+import math;
+x1=float(input("x1="));
+y1=float(input("y1="));
+r=math.sqrt(x1*x1+y1*y1);
+h1=float(input("h1="));
+h2=float(input("h2="));
+l2=float(input("l2="));
+l3=float(input("l3="));
+l4=float(input("l4="));
+from scipy.optimize import fsolve
+ 
+def solve_function(unsolved_value):
+    x,y,p,q=unsolved_value[0],unsolved_value[1],unsolved_value[2],unsolved_value[3]
+    return [
+        x*l4+q*l3-r-l2,
+        p*l3-y*l4+h2-h1,
+        x**2+y**2-1,
+        p**2+q**2-1,
+    ]
+ 
+solved=fsolve(solve_function,[0, 0, 0, 0])
+print(solved)
+print(math.acos(solved[0]))
+print(math.acos(solved[2])) 
+ 
+print("Program done!")
